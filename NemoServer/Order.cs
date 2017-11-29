cadf9716d9a3d8192c2664ad11507656e5351678
Philippe Berglund <philippe05@gmail.com>
2017-11-29 15:52:18 +0100

    v1.0 presale

---
+++ NemoServer/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NemoServer
+{
+    class Order
+    {
+        public int OrderNumber { get; set; }
+        public string DishName { get; set; }
+        public bool ReadyStatus { get; set; }
+    }
+}
